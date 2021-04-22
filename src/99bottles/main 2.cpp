b2b2cbbae9631586c6d9d4c2f38de1128d1458e4
lucinda27 <81180210+lucinda27@users.noreply.github.com>
2021-04-22 12:39:22 -0600

    Add files via upload

--- src/99bottles/main 2.cpp
+++ src/99bottles/main 2.cpp
@@ -6,8 +6,13 @@ int main () {
    int a = 99;
 
    // while loop execution
-   while( a > 1 ) {
+   while( a > 2 ) {
       cout << a << " bottles of milk on the wall, " << a << " bottles of milk" <<"\n Take one down, pass it around, " <<a-1<< " bottles of milk on the wall" <<endl;
+      a--;
+   }
+
+   while( a > 1 ) {
+      cout << a << " bottles of milk on the wall, " << a << " bottles of milk" <<"\n Take one down, pass it around, " <<a-1<< " bottle of milk on the wall" <<endl;
       a--;
    }
 
