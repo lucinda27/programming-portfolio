fe3cd1e679aeac515a3680210138f34b2959f7d7
lucinda27 <81180210+lucinda27@users.noreply.github.com>
2021-06-03 09:30:21 -0600

    Create Object.h

---
+++ src/Final project/Object.h
@@ -0,0 +1,17 @@
+#ifndef Room_H
+#define Room_H
+#include <string>
+using namespace std;
+
+class Object
+{
+      private:
+      string description;
+
+      public:
+      Object(string d);
+      string setDescription();
+      
+
+};
+#endif
